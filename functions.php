5d6daaca637e5deb88656d04cebbdaa2323060a5
MaryKenney <mary.kenney@mavs.uta.edu>
2022-05-09 14:52:52 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+	$header = "<header>\n
+    <div class='container header'>\n
+        <a href='index.php'>\n
+            <span class='navbar-brand'></span>\n
+        </a>\n
+        <button class='navbar-toggle' type='button' data-target='#main-navigation'>\n
+            <i class='fas fa-bars'></i>\n
+        </button>\n
+        <ul id='main-navigation' class='navbar'>\n
+            <li><a href='index.php' title='Home'>Home</a></li>\n
+            <li><a href='about.php' title='About'>About</a></li>\n
+            <li><a href='partner.php' title='Partner'>Partner</a></li>\n
+            <li><a href='get-involved.php' title='Get Involved'>Get Involved</a></li>\n
+            <li><a href='register.php' title='Register a Student'>Register a Student</a></li>\n
+        </ul>\n
+        </nav>\n
+    </div>\n
+</header>\n";
+
+$footer = "<footer>\n
+<div class='container'>\n
+    <div class='row' id='footer-space'>\n
+        <a href='index.php'><img class='col-2' id='ft-logo' src='img/logo.webp' alt='Young STEM Professionals Logo'></a>\n
+        <div class='col-2'>\n
+            <div class='ft-menu'>\n
+                <p><b>Important Links</b></p>\n
+                <ul class='onespc'>\n
+                    <li><a href='index.php' title='Home'>Home</a></li>\n
+                    <li><a href='about.php' title='About'>About</a> </li>\n
+                    <li><a href='partner.php' title='Partner'>Partner</a></li>\n
+                    <li><a href='get-involved.php' title='Get Involved'>Get Involved</a></li>\n
+                    <li><a href='register.php' title='Register a Student'>Register a Student</a></li>\n
+                </ul>\n
+            </div>\n
+        </div>\n
+        <div class='col-2'>\n
+            <p><b>Description</b></p>\n
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum consequat auctor.</p>\n
+        </div>\n
+        <div class='col-2'>\n
+            <p><b>Contact Us</b></p>\n
+            <p>add social links later</p>\n
+        </div>\n
+    </div>\n
+</div>\n
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js' integrity='sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==' crossorigin='anonymous'></script>\n
+<script src='js/app.js'></script>\n
+</footer>\n";
+?>
